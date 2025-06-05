adaf0a12bd4bd1b23739e195b733050f4f1ec5f7
Mahmoud Mansy <mahmoudmohamedk8200949@gmail.com>
2025-06-05 04:18:49 +0300

    add

--- components/youtube/VideoPlayer.tsx
+++ components/youtube/VideoPlayer.tsx
@@ -487,7 +487,7 @@ export function VideoInfo({
   isExpanded = false,
   locale,
   messages,
-  playlist = null,
+  playlist,
 }: VideoInfoProps) {
   const [showFullDescription, setShowFullDescription] = useState(isExpanded);
 
